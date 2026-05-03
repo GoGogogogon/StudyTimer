5d21ccbca1cdde894126886ee32c2bf6e1a69b4b
orimono <hayaome123@gmail.com>
2026-05-03 21:26:55 +0900

    first commit

---
+++ front/app.js
@@ -0,0 +1,148 @@
+// タイマーのデータと機能をまとめたオブジェクト
+
+
+const startbtn = document.querySelector("#start-btn");
+console.log("ボタンの確認：",startbtn);
+startbtn.addEventListener('click', ()=>{
+    studyTimer.start();
+});
+ 
+const stopbtn = document.querySelector('#stop-btn');
+
+stopbtn.addEventListener('click',()=>{
+    studyTimer.stop();
+});
+
+const resetbtn = document.querySelector('#reset-btn');
+
+resetbtn.addEventListener('click',()=>{
+    studyTimer.reset();
+});
+
+const audio = new Audio("アナウンス前のピンポンパンポン.mp3");
+
+const studylog = document.querySelector(`#log-list`);
+const studyTimer = {
+
+    timeLeft: 1500,  // 初期の残り時間
+    timerInterval: null, // タイマーのID
+  
+    // 1. タイマーを開始するメソッド
+    start() {
+
+            const input_data = document.querySelector("#subject-input");
+            const display_time = document.querySelector("#time-display");
+
+            if(input_data.value === ""){
+                alert("科目を入れてください");
+                return -1;
+              }
+
+            if(this.timerInterval){
+                 return -1;
+             }   
+            this.timerInterval = setInterval(() => {
+              
+                this.timeLeft -= 1;
+
+                const minute = this.timeLeft / 60;
+
+                const second = this.timeLeft % 60;
+
+                display_time.textContent = `${Math.floor(minute)}:${second.toString().padStart(2,"0")}`;
+
+                console.log("残り時間:",this.timeLeft);
+
+                if(this.timeLeft === 0){
+                clearInterval(this.timerInterval);
+                audio.play();
+                this.savelog(input_data);
+                this.rest();
+              }
+            },10);
+
+
+    },
+
+    // タイマーを止めるメソッド
+    stop() {
+        clearInterval(this.timerInterval);
+        console.log("タイマー停止。残り：", this.timeLeft);
+        this.timerInterval = null;
+    },
+    //タイマーをリセットするメソッド
+    reset() {
+        clearInterval(this.timerInterval);
+        const display_time = document.querySelector("#time-display");
+        display_time.textContent = "25:00";
+        console.log("リセットします. タイムの初期化",this.timeLeft = 1500);
+        this.timerInterval = null;
+
+    },
+    //休憩メソッド(5分間)
+    rest(){
+        clearInterval(this.timerInterval);
+        this.timerInterval = null;
+
+        this.timeLeft  = 300;
+
+        const display_time = document.querySelector("#time-display");
+
+        display_time.textContent = "5:00";
+
+
+       
+        this.timerInterval = setInterval(()=>{
+            
+            
+            console.log(this.timeLeft);
+            
+            
+            const minute = this.timeLeft / 60;
+
+            const second = this.timeLeft % 60;
+
+            display_time.textContent = `${Math.floor(minute)}:${second.toString().padStart(2,"0")}`;
+
+            this.timeLeft -= 1;
+
+            if(this.timeLeft === 0) {
+                audio.play();
+                display_time.textContent= "お疲れさまでした！"; 
+                clearInterval(this.timerInterval);
+            }
+        },10)
+    
+
+    },
+
+    async savelog(input_data){
+
+    
+        const dataToSend = {
+            subject: input_data.value,
+            study_time : 25
+        };
+        studylog.append(input_data.value);
+        
+        try {
+            const response = await fetch("/api/save",{
+                method : "POST",
+                headers : {
+                    "Content-Type" : "application/json"},
+                    body : JSON.stringify(dataToSend)
+                    
+            });
+
+        if(response.ok) {
+            console.log("成功");
+        } else {
+            console.log("サーバーエラー",response.status);
+            alert("保存できませんでした");
+        }   
+    } catch(error) {
+        console.log("接続失敗",error);
+    }
+    }
+
+}
---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type StudyLog struct {
+	Title string `json:"subject"`
+	Time  int    `json:"study_time"`
+}
+
+var db *sql.DB
+
+func main() {
+
+	var err error
+	dsn := "root:root@tcp(127.0.0.1:3306)/study_timer"
+
+	db, err = sql.Open("mysql", dsn)
+
+	if err != nil {
+		log.Fatal("設定エラー：", err)
+	}
+
+	defer db.Close()
+
+	for i := 0; i < 3; i++ {
+		err = db.Ping()
+		if err == nil {
+			break
+		}
+		fmt.Println("待機中")
+		time.Sleep(2 * time.Second)
+	}
+
+	if err != nil {
+		log.Fatal("接続失敗...:", err)
+	}
+
+	fmt.Println("接続成功しました")
+
+	const createTableSQL = `CREATE TABLE IF NOT EXISTS study_logs (
+        id          INT AUTO_INCREMENT PRIMARY KEY, 
+        subject     VARCHAR(255) NOT NULL,
+        study_time  INT NOT NULL,
+        created_at  DATETIME DEFAULT CURRENT_TIMESTAMP
+    );`
+
+	_, err = db.Exec(createTableSQL)
+	if err != nil {
+		log.Fatal("テーブル作成失敗:", err)
+	}
+
+	fmt.Println("テーブル作成成功（もしくはすでにある)")
+
+	http.HandleFunc("/api/save", saveHandler)
+
+	fileServer := http.FileServer(http.Dir("./front"))
+	http.Handle("/", fileServer)
+
+	fmt.Println("サーバーを起動しました！ http://localhost:8080")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("サーバー起動失敗:", err)
+	}
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+
+	// 【課題】ここに「保存処理」を書く
+	if r.Method != http.MethodPost {
+		http.Error(w, "POST以外は受け取りません", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var a StudyLog
+
+	err := json.NewDecoder(r.Body).Decode((&a))
+	if err != nil {
+		log.Print("Jsonの読み込みエラー")
+		http.Error(w, "読み込みエラーが生じました", http.StatusBadRequest)
+		return
+	}
+
+	const insertSQL = `INSERT INTO study_logs (subject,study_time) VALUES(?,?)`
+
+	_, err = db.Exec(insertSQL, a.Title, a.Time)
+
+	if err != nil {
+		log.Print("dbに格納できません")
+		http.Error(w, "データが挿入できません", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println("データ挿入成功")
+	w.Write([]byte("保存しました!"))
+	fmt.Println("JSからアクセスが来ました") // 確認用ログ
+}
