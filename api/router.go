9d1f761d6ac868098c04aa3c137d04d96e063b1a
orimono <hayaome123@gmail.com>
2026-05-05 01:40:19 +0900

    main層の改善

--- api/router.go
+++ api/router.go
@@ -1,1 +1,18 @@
 package api
+
+import (
+	"SmartStudyTimer/service"
+	"database/sql"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func NewRouter(db *sql.DB) *mux.Router {
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/save", service.SaveHandler).Methods(http.MethodPost)
+
+	return r
+}
